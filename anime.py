1b9984acb3e4e36c31be919f915f497be4247d28
arya5691 <gowripsundaram@gmail.com>
2015-08-08 16:02:51 +0530

    Adding correct script.

--- anime.py
+++ anime.py
@@ -1,61 +1,66 @@
 import argparse
 from bs4 import BeautifulSoup
-import cookielib
 import httplib
 import logging
-import os.path
 from random import randint
+import re
 import sys
-import urllib
 import urllib2
 
-COOKIEFILE = 'cookies.lwp'
 MIN_ID=1
 MAX_ID=11000
 
 class Anime(object):
   id = -1
   title = ""
+  arithmeticMean = -1
+  weightedMean = -1
 
-  def __init__ (self, id, title):
+  def __init__ (self, id, title, arithmeticMean, weightedMean):
     self.id = id
     self.title = title
+    self.arithmeticMean = arithmeticMean
+    self.weightedMean = weightedMean
 
-def lookupMAL(title, userId, password):
-  url = "http://%s:%s@myanimelist.net/" % (userId, password)
+# Returns the Anime object represented by the anime id as per
+# Anime News Network (ANN).
+# Returns 'None' if:
+#   - the entry corresponding to this id does not exist, OR
+#   - this id represents a Manga entry, OR
+#   - if the anime does not have any associated rating attribute, OR
+#   - either the arithmetic or the weighted mean is lower than |threshold|.
+def getAnime(animeId, threshold):
+  url = "http://www.animenewsnetwork.com/encyclopedia/anime.php?id=%s" % str(animeId)
+  try: 
+    response = urllib2.urlopen(url)
+    html = response.read()
+    parsedHtml = BeautifulSoup(html, "lxml")
 
-# Returns the title represented by the anime id as per Anime News Network (ANN).
-# Returns 'None' if the entry corresponding to this id does not exist, or
-# represents a Manga entry.
-def getTitle(animeId):
-  animeId = 1408
-  url = "http://myanimelist.net/anime.php?id=%s" %  str(animeId)
-  username = "arya5691"
-  password = "kind>reqd<3"
+    title =  parsedHtml.find("div", {"id": "page-title"}).find("h1", {"id": "page_header"}).contents[0]
+    # Exclude manga entries.
+    if 'manga' in title:
+      return None
+    
+    ratingText =  parsedHtml.find("div", {"id": "ratingbox"})
+    # Exclude entries with no ratings.
+    if ratingText is None:
+      return None
 
-  cj = cookielib.CookieJar()
-  opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-  login_data = urllib.urlencode({'username' : username, 'j_password' : password})
-  resp = opener.open(url, login_data)
-  print resp.read()
-  resp = opener.open(url)
-  print resp.read()
-  for index, cookie in enumerate(cj):
-    print index, '  :  ', cookie
-  try:
-    print "a"
-# print response.info()
-#parsedHtml = BeautifulSoup(html, "lxml")
-#   print parsedHtml# .find("div", {"id": "contentWrapper"}) # .find("h1", {"id": "page_header"}).contents[0]
-#   for index, cookie in enumerate(cj):
-#    print index, '  :  ', cookie
-#   cj.save(COOKIEFILE)
+    arithmeticMean = float(re.findall(b'<b>Arithmetic mean:</b> (\d+.\d+)', str(ratingText))[0]);
+    weightedMean = float(re.findall(b'<b>Weighted mean:</b> (\d+.\d+)', str(ratingText))[0]);
+    # Exclude entries with low ratings.
+    if arithmeticMean < threshold or weightedMean < threshold:
+      return None
+
+    anime = Anime(animeId, title, arithmeticMean, weightedMean)
+    return anime
+
   except urllib2.HTTPError, e:
     logging.warning('HTTPError = %s, animeId = %s' % (str(e.code), str(animeId)))
   except urllib2.URLError, e:
     logging.warning('URLError = %s, animeId = %s' % (str(e.reason), str(animeId)))
   except httplib.HTTPException, e:
-    logging.warning('HTTPException, animeId = %s' % str(e))
+    logging.warning('HTTPException, animeId = %s' % str(animeId))
   except Exception:
     import traceback
     logging.warning('generic exception: ' + traceback.format_exc())
@@ -65,23 +70,18 @@ def getRandomId():
   return randint(MIN_ID, MAX_ID)
 
 if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--threshold', required=True, type=int, help="Minimum allowed rating as per AnimeNewsNetwork (ANN)")
+  args = parser.parse_args()
+  print "Threshold: %s" % args.threshold
 
   visited = set()
-
   while (True):
     animeId = getRandomId()
     if (animeId in visited):
       continue
     visited.add(animeId)
-    animeTitle = getTitle(animeId)
-    print animeTitle
-    break
-      
-  parser = argparse.ArgumentParser()
-  # username + password.
-  # Allow movies?
-  parser.add_argument('--threshold', required=True, type=int, help="Minimum allowed rating as per MyAnimeList")
-  args = parser.parse_args()
-  print args
-
-  print getTitle(100)
+    anime = getAnime(animeId, args.threshold)
+    if anime is None:
+      continue
+    print str(anime.__dict__)
